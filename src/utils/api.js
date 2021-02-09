14531a2c1c6deb809587a7c6cd7ff516cdbe042c
HsiaoFei <52922361+HsiaoFei@users.noreply.github.com>
2021-02-09 11:12:34 +0800

    Update api.js

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+const ApiRootUrl = '';
+const RootUrl = '';
+const ApiUrl = '';
+
+module.exports = {
+  GuidUrl: ApiRootUrl + '', //区服
+  NormalUrl: ApiRootUrl + '', //信息
+  ListUrl: RootUrl + '', //比赛列表
+  MatchUrl: RootUrl + '', //对局详情
+  ImageUrl: RootUrl + '', //图片
+  
+  NoticeUrl: ApiUrl + '', //公告
+  UpdateUrl: ApiUrl + '', //更新日志
+
+
+};
