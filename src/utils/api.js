753375add0129f56d3de5740c1f56d0953fb7e7d
HsiaoFei <52922361+HsiaoFei@users.noreply.github.com>
2021-04-18 23:45:29 +0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,19 @@
-const ApiRootUrl = '';
-const RootUrl = '';
-const ApiUrl = '';
+const ApiRootUrl = 'https://m300wxapp.jumpw.com/';
+const RootUrl = 'https://300report.jumpw.com/';
+const ApiUrl = 'https://hsiaofei.com/';
 
 module.exports = {
-  GuidUrl: ApiRootUrl + '', //区服
-  NormalUrl: ApiRootUrl + '', //信息
-  ListUrl: RootUrl + '', //比赛列表
-  MatchUrl: RootUrl + '', //对局详情
-  ImageUrl: RootUrl + '', //图片
-  
-  NoticeUrl: ApiUrl + '', //公告
-  UpdateUrl: ApiUrl + '', //更新日志
+  GuidUrl: ApiRootUrl + 'config/getGuid', //区服
+  NormalUrl: ApiRootUrl + 'battle/searchNormal', //信息
+  ListUrl: RootUrl + 'api/getlist', //比赛列表
+  MatchUrl: RootUrl + 'api/getmatch', //对局详情
+  ImageUrl: RootUrl + 'static/images/', //图片
 
+  NoticeUrl: ApiUrl + 'file/wechat/Hero300/notice', //公告
+  UpdateUrl: ApiUrl + 'file/wechat/Hero300/update', //更新日志
+
+  // MatchsUrl: ApiRootUrl + 'battle/searchMatchs', //比赛列表
+  // MatchinfoUrl: ApiRootUrl + 'battle/searchMatchinfo', //对局详情
+  // HeroRankUrl: ApiRootUrl + 'battle/searchHeroRank', //排行榜
 
 };
